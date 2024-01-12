fb6b88501da8bc0bcff5b5da2c097e6468a121b9
belloLucas <lucasbellodev@gmail.com>
2024-01-12 16:32:33 -0300

    Aula 04 concluida

---
+++ 04/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const text = "hello world"
+
+type ID int
+
+var (
+	b bool    = true
+	c int     = 24
+	d string  = "Lucas"
+	e float64 = 21.10
+	f ID      = 1
+)
+
+func main() {
+	fmt.Printf("O tipo de E é %T", e)
+}
